aa0dd0f9b72c61ad4b1e2cc1f26224ce45436803
wroveda <walter.roveda@posteo.it>
2022-05-05 18:43:34 +0200

    Add default theme selector

--- src/assets/js/themes.js
+++ src/assets/js/themes.js
@@ -1,10 +1,23 @@
 // Toggles between dark and light mode
 function switchTheme() {
-	document.querySelector("body").classList.toggle("dark-mode");
+	document.querySelector("html").classList.toggle("dark-mode");
 	
 	// Switches icons
 	document.querySelector("#sun").classList.toggle("hidden");
 	document.querySelector("#moon").classList.toggle("hidden");
+	
+	// Sets theme preference in the browser
+	localStorage.setItem(
+		"dark-mode",
+		document.querySelector("html").classList.contains("dark-mode")
+	);
+}
+
+// Switches to dark theme if it is prefered
+if (window.matchMedia("(prefers-color-scheme: dark)").matches ||
+	localStorage.getItem("dark-mode") == "true") {
+	
+	switchTheme();
 }
 
 // --------------- BUTTONS ------------------
